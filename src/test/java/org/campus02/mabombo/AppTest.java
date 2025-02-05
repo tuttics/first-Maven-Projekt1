e8e8a18c26405a158aa0ce1fb93ed36183d71034
tuttics <52403815@edu.campus02.at>
2025-02-05 20:34:47 +0100

    1. Ich habe die Klasse BMICalculator mit den angeforderten Attributen, Methoden und Konstruktor erstellt
    2. Die Testklasse habe ich nun erstellt
    
    3. Testversuch erfolgreich durchgeführt

--- src/test/java/org/campus02/mabombo/AppTest.java
+++ src/test/java/org/campus02/mabombo/AppTest.java
@@ -1,38 +1,19 @@
 package org.campus02.mabombo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest
-    extends TestCase
 {
     /**
-     * Create the test case
-     *
-     * @param testName name of the test case
+     * Rigorous Test :-)
      */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+    @Test
     public void testApp()
     {
-        assertTrue( true );
+        assertTrue(true);
     }
 }
