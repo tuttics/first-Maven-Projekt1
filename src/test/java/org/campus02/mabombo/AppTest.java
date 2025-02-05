9be6492f811aa6c05b8ee955e6edfb6d732eeb26
tuttics <52403815@edu.campus02.at>
2025-02-05 20:35:16 +0100

    1. Ich habe die Klasse BMICalculator mit den angeforderten Attributen, Methoden und Konstruktor erstellt
    2. Die Testklasse habe ich nun erstellt
    
    3. Testversuch erfolgreich durchgeführt - die Kommentare gelöscht

--- src/test/java/org/campus02/mabombo/AppTest.java
+++ src/test/java/org/campus02/mabombo/AppTest.java
@@ -3,14 +3,10 @@ package org.campus02.mabombo;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
     public void testApp()
     {
